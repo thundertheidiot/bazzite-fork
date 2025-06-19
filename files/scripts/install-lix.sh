61cb6cf5e6fdd3be9dbd3525cee671284df0c658
Thunder <thunder@disroot.org>
2025-06-19 14:15:54 +0300

    lix setup attempt #2

---
+++ files/scripts/install-lix.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# lix installer
+curl -sSf -L https://install.lix.systems/lix | sh -s -- install linux --no-start-daemon --no-confirm
+
+# setup service files, cross fs symlinks don't work
+rm /etc/systemd/system/nix-daemon.service /etc/systemd/system/nix-daemon.socket
+cp /nix/var/nix/profiles/default/lib/systemd/system/* /etc/systemd/system/
+ln -sf /etc/systemd/system/nix-daemon.socket /etc/systemd/system/sockets.target.wants/nix-daemon.socket
+
+groupadd nixbld
+
+# setup nix directory
+mv /nix /etc/.tmp-nix
+mkdir /nix
