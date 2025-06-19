97efa676a7ae1a4be12e13fd4f849ca258c5c5da
Thunder <thunder@disroot.org>
2025-06-19 18:25:44 +0300

    fix lix install once again, rename bazzite recipe

--- files/scripts/install-lix.sh
+++ files/scripts/install-lix.sh
@@ -9,6 +9,15 @@ rm /etc/systemd/system/nix-daemon.service /etc/systemd/system/nix-daemon.socket
 cp /nix/var/nix/profiles/default/lib/systemd/system/* /etc/systemd/system/
 ln -sf /etc/systemd/system/nix-daemon.socket /etc/systemd/system/sockets.target.wants/nix-daemon.socket
 
+# This cannot be done as a system file, the lix installer fails otherwise.
+mkdir -p /etc/systemd/system/nix-daemon.socket.d
+cat <<EOF > /etc/systemd/system/nix-daemon.socket.d/override.conf
+[Unit]
+After=nix.mount
+BindsTo=nix.mount
+EOF
+
+
 # setup nix directory
 mv /nix /etc/.tmp-nix
 mkdir /nix
